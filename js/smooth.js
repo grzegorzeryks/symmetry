024d83ba5b7951e496a01f28e14b5ae01a3870e4
grzegorzeryks <grzegorzeryks@gmail.com>
2018-07-17 19:52:02 +0200

    smoothscroll

---
+++ js/smooth.js
@@ -0,0 +1,20 @@
+ scrollTo = (element) => {
+  window.scroll({
+    behavior: 'smooth',
+    left: 0,
+    top: element.offsetTop
+
+  });
+};
+
+document.querySelector('.bio-menu').addEventListener('click', () => {
+  scrollTo(document.getElementById("bio"));
+});
+
+document.querySelector('.coaching-menu').addEventListener('click', () => {
+  scrollTo(document.getElementById("coaching"));
+});
+
+document.querySelector('.contact-menu').addEventListener('click', () => {
+  scrollTo(document.getElementById("contact"));
+});
