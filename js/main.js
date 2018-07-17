c10e8f0973d321ef5deb786ce0b9594cb7894cca
grzegorzeryks <grzegorzeryks@gmail.com>
2018-07-17 15:59:44 +0200

    coaching section

--- js/main.js
+++ js/main.js
@@ -2,12 +2,21 @@ console.log('elo');
 
 let personalCLick = document.querySelector('.oneonone');
 let personalBox = document.querySelector('.personal')
+let insideContent = document.querySelector('.inside-content')
+console.log(insideContent);
+personalCLick.addEventListener('click', showBox);
 
-console.log(personalBox);
-personalCLick.addEventListener('click', function(){
-console.log('works');
-personalBox.style.left = '0';
-personalBox.style.transition = 'all 0.8s';
-});
+personalBox.addEventListener('click', hideBox);
+
+function hideBox(){
+  personalBox.style.left = '-2000px';
+}
+
+
+function showBox(){
+  console.log('works');
+  personalBox.style.left = '0';
+  personalBox.style.transition = 'all 0.8s';
+}
 
 // defend.addEventListener('click', defenseAction);
